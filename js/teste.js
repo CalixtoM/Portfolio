9806f23e2d8077b5d63f7839b9a1ef827a941fbb
Matheus Calixto <51165259+CalixtoM@users.noreply.github.com>
2023-07-14 10:14:18 -0300

    novo background

---
+++ js/teste.js
@@ -0,0 +1,13 @@
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = ' ';
+    textoArray.forEach(function(letra, i){   
+      
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 75 * i)
+
+  });
+}
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
